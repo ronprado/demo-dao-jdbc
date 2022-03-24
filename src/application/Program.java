a91752e47b322427994e36a75df5bac481e7598a
ronaldopradoartes@gmail.com <ronaldopradoartes@gmail.com>
2022-03-24 07:33:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,8 +35,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(0), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Incerted! New id: " + newSeller.getId());
-	    }
 		
+		
+        System.out.println("\n=== TESTE 5: Seller Update ===");
+	     seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+	    }	
 	}
 	
 
