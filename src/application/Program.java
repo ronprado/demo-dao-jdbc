a374daba1ee5a6e79534dde44b5d66c61eda1b4c
ronaldopradoartes@gmail.com <ronaldopradoartes@gmail.com>
2022-03-24 08:06:32 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -42,6 +44,14 @@ public class Program {
         seller.setName("Marta Waine");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+        
+        System.out.println("\n=== TESTE 6: Seller Delete ===");
+        System.out.println("Enter id code for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+        
+        sc.close();
 	    }	
 	}
 	
